e50841a7904aea3a35aeb96c084121dc9c4d01a4
kyfxbl <kyfxbl@gmail.com>
2014-08-14 10:29:42 +0800

    继续搭建框架

--- src/weixin/service/msite.js
+++ src/weixin/service/msite.js
@@ -2,13 +2,15 @@ var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 var _ = require("underscore");
 
 exports.jumpToWShop = jumpToWShop;
+exports.jumpToWMember = jumpToWMember;
+exports.jumpToWActivity = jumpToWActivity;
 
 function jumpToWShop(req, res, next){
 
     var enterprise_id = req.query["enterprise_id"];
 
     if(!enterprise_id){
-        res.render("shop_error", {layout: "error_layout", errorCode: 400, errorMessage:"请求参数错误"});
+        res.render("site_error", {layout: "error_layout", errorCode: 400, errorMessage:"请求参数错误"});
         return;
     }
 
@@ -16,7 +18,7 @@ function jumpToWShop(req, res, next){
 
         if(err){
             console.log(err);
-            res.render("shop_error", {layout: "error_layout", errorCode: 500, errorMessage:"数据库访问错误"});
+            res.render("site_error", {layout: "error_layout", errorCode: 500, errorMessage:"数据库访问错误"});
             return;
         }
 
@@ -26,6 +28,14 @@ function jumpToWShop(req, res, next){
             members.push({name: item.name});
         });
 
-        res.render("shop_success", {members: members});
+        res.render("shop", {members: members});
     });
+}
+
+function jumpToWMember(req, res, next){
+
+}
+
+function jumpToWActivity(req, res, next){
+
 }
