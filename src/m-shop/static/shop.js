aacfb2912b5b1da2f88124aa667b784ed851a818
kyfxbl <kyfxbl@gmail.com>
2014-08-14 18:17:26 +0800

    用zepto函数操作DOM

--- src/m-shop/static/shop.js
+++ src/m-shop/static/shop.js
@@ -4,6 +4,7 @@ $(function(){
         title: '标签 from art_template',
         list: ['文艺', '博客', '摄影', '电影', '民谣', '旅行', '吉他']
     };
-    var html = template('test', data);
-    document.getElementById('content').innerHTML = html;
+    var content = template('test', data);
+
+    $("#content").html(content);
 });
