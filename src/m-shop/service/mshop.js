90e440340732bc8db49be4f2331c9cec744cc6b8
kyfxbl <kyfxbl@gmail.com>
2014-08-14 16:20:45 +0800

    框架，把每个模块分开

--- src/m-shop/service/mshop.js
+++ src/m-shop/service/mshop.js
@@ -2,15 +2,14 @@ var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 var _ = require("underscore");
 
 exports.jumpToWShop = jumpToWShop;
-exports.jumpToWMember = jumpToWMember;
-exports.jumpToWActivity = jumpToWActivity;
 
 function jumpToWShop(req, res, next){
 
     var enterprise_id = req.query["enterprise_id"];
 
     if(!enterprise_id){
-        res.render("site_error", {layout: "error_layout", errorCode: 400, errorMessage:"请求参数错误"});
+
+        next({errorCode: 400, errorMessage:"请求参数错误"});
         return;
     }
 
@@ -18,7 +17,7 @@ function jumpToWShop(req, res, next){
 
         if(err){
             console.log(err);
-            res.render("site_error", {layout: "error_layout", errorCode: 500, errorMessage:"数据库访问错误"});
+            next({errorCode: 500, errorMessage:"数据库访问错误"});
             return;
         }
 
@@ -28,14 +27,6 @@ function jumpToWShop(req, res, next){
             members.push({name: item.name});
         });
 
-        res.render("shop", {members: members});
+        res.render("mshop", {members: members});
     });
-}
-
-function jumpToWMember(req, res, next){
-
-}
-
-function jumpToWActivity(req, res, next){
-
 }
