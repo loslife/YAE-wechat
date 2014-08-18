ef6d480f6a76a7a103089cb082311d6c5eff2c31
kyfxbl <kyfxbl@gmail.com>
2014-08-18 17:00:29 +0800

    控制微信右上角的按钮，会员和登陆不需要分享

--- src/m-shop/static/shop.js
+++ src/m-shop/static/shop.js
@@ -1,3 +1,18 @@
+function onBridgeReady(){
+    WeixinJSBridge.call('showOptionMenu');
+}
+
+if (typeof WeixinJSBridge == "undefined"){
+    if(document.addEventListener){
+        document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+    }else if (document.attachEvent){
+        document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+        document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+    }
+}else{
+    onBridgeReady();
+}
+
 $(function(){
 
     var data = {
