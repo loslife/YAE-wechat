51de56b13912088b61a1a8881641aed82fc136da
kyfxbl <kyfxbl@gmail.com>
2014-08-26 19:09:39 +0800

    全局目录

--- src/weixin-binding/static/binding.js
+++ src/weixin-binding/static/binding.js
@@ -1,3 +1,14 @@
 $(function(){
 
+    $("#getSecurityCode").tap(function(){
+
+        var phone = "15019432710";
+
+        var url = "http://192.168.1.124:3000/svc/getCode/" + phone + "?u=weixin_binding";
+
+        $.get(url, function(response){
+            alert("ok");
+        })
+    });
+
 });
