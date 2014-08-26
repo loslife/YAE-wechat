d54c580631f865ce482df3636f802dcb59b7b070
kyfxbl <kyfxbl@gmail.com>
2014-08-26 19:15:55 +0800

    发送验证码的地址，写在全局变量里

--- src/resource/static/wechat_global.js
+++ src/resource/static/wechat_global.js
@@ -1,1 +1,3 @@
-alert("ok");
+var g_env = {
+    security_code_url: "http://192.168.1.124:3000/svc/getCode/"
+};
--- src/weixin-binding/static/binding.js
+++ src/weixin-binding/static/binding.js
@@ -4,7 +4,7 @@ $(function(){
 
         var phone = "15019432710";
 
-        var url = "http://192.168.1.124:3000/svc/getCode/" + phone + "?u=weixin_binding";
+        var url = g_env.security_code_url + phone + "?u=weixin_binding";
 
         $.get(url, function(response){
             alert("ok");
