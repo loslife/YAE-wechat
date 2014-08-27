b292e5c448112cec4da7ca1f5d60a30f426dbe35
kyfxbl <kyfxbl@gmail.com>
2014-08-27 12:52:03 +0800

    绑定通道打通

--- src/resource/static/wechat_global.js
+++ src/resource/static/wechat_global.js
@@ -1,3 +1,5 @@
 var g_env = {
-    security_code_url: "http://192.168.1.124:3000/svc/getCode/"
+    security_code_url: "http://192.168.1.124:3000/svc/getCode/",
+    check_code_url: "http://192.168.1.124:3000/svc/checkCode/",
+    binding_url: "http://192.168.1.124:5000/svc/wsite/"
 };
--- src/weixin-binding/static/binding.js
+++ src/weixin-binding/static/binding.js
@@ -2,13 +2,47 @@ $(function(){
 
     $("#getSecurityCode").tap(function(){
 
-        var phone = "15019432710";
+        var phone = $("#phone").val();
+
+        if(!phone){
+            alert("请输入手机号");
+            return;
+        }
 
         var url = g_env.security_code_url + phone + "?u=weixin_binding";
 
         $.get(url, function(response){
-            alert("ok");
-        })
+            alert("验证码已发送到您的手机");
+        });
+    });
+
+    $("#btn_binding").tap(function(){
+
+        var phone = $("#phone").val();
+        var secure_code = $("#code").val();
+
+        if(!phone || !secure_code){
+            alert("输入不能为空");
+            return;
+        }
+
+        var url = g_env.check_code_url + phone + "?u=weixin_binding&c=" + secure_code;
+        $.get(url, function(response){
+
+            var code = response.code;
+            if(code !== 0){
+                alert("验证码错误");
+                return;
+            }
+
+            var enterprise_id = $("#enterprise_id").text();
+            var open_id = $("#open_id").text();
+
+            var bindingURL = g_env.binding_url + enterprise_id + "/doBinding";
+            $.post(bindingURL, { open_id: open_id, phone: phone }, function(response){
+                alert(response);
+            })
+        });
     });
 
 });
