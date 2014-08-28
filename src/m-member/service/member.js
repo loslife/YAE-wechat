ebe00b21a650ad5b4e66171f622581343a1abfe9
kyfxbl <kyfxbl@gmail.com>
2014-08-28 19:49:57 +0800

    查询会员卡信息服务

--- src/m-member/service/member.js
+++ src/m-member/service/member.js
@@ -3,15 +3,27 @@ var _ = require("underscore");
 
 exports.jumpToWMember = jumpToWMember;
 exports.checkSession = checkSession;
+exports.queryMemberCardInfo = queryMemberCardInfo;
 
 function jumpToWMember(req, res, next) {
-    var enterprise_id = req.params.enterpriseId;
 
+    var enterprise_id = req.params.enterpriseId;
     res.render("member", {name: "会员一号", enterprise_id: enterprise_id, menu: "member"});
 }
 
+function queryMemberCardInfo(req, res, next){
+
+    var enterprise_id = req.body.enterprise_id;
+    var member_id = req.body.member_id;
+
+    console.log(enterprise_id);
+    console.log(member_id);
+
+    _response(req, res, 0, {message: "ok"});
+}
 
 function checkSession(req, res, next) {
+
     var enterprise_id = req.params["enterpriseId"];
 
     if (req.session && req.session.member_id) {
@@ -20,4 +32,13 @@ function checkSession(req, res, next) {
     }
 
     res.redirect("/svc/wsite/" + enterprise_id + "/login");
+}
+
+function _response(req, res, code, resultEntity){
+
+    var response = {
+        code: code,
+        result: resultEntity
+    };
+    doResponse(req, res, response);
 }
