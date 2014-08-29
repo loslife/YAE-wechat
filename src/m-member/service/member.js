a1b5060bb86afce18e3ed68f49c33ca3bcef1f02
kyfxbl <kyfxbl@gmail.com>
2014-08-29 16:35:59 +0800

    会员卡查询的框架

--- src/m-member/service/member.js
+++ src/m-member/service/member.js
@@ -1,5 +1,6 @@
 var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
 var _ = require("underscore");
+var async = require("async");
 
 exports.jumpToWMember = jumpToWMember;
 exports.checkSession = checkSession;
@@ -9,31 +10,6 @@ function jumpToWMember(req, res, next) {
 
     var enterprise_id = req.params["enterpriseId"];
     res.render("member", {name: "会员一号", enterprise_id: enterprise_id, menu: "member"});
-}
-
-function queryMemberCardInfo(req, res, next){
-
-    var enterprise_id = req.params["enterpriseId"];
-    var member_id = req.body.member_id;
-
-    dbHelper.queryData("tb_member", {enterprise_id: enterprise_id, id: member_id}, function(err, result){
-
-        if(err){
-            _response(req, res, 1, {errorMessage: "数据库异常"});
-            return;
-        }
-
-        if(result.length === 0){
-            _response(req, res, 1, {errorMessage: "会员不存在"});
-            return;
-        }
-
-        var cards = [];
-        cards.push({name: "金卡", money: 232});
-        cards.push({name: "钻石卡", money: 1944});
-        cards.push({name: "美丽一夏卡", money: 2000});
-        _response(req, res, 0, cards);
-    });
 }
 
 function checkSession(req, res, next) {
@@ -48,11 +24,63 @@ function checkSession(req, res, next) {
     res.redirect("/svc/wsite/" + enterprise_id + "/login");
 }
 
-function _response(req, res, code, resultEntity){
+function queryMemberCardInfo(req, res, next){
 
-    var response = {
-        code: code,
-        result: resultEntity
-    };
-    doResponse(req, res, response);
+    var enterprise_id = req.params["enterpriseId"];
+    var member_id = req.body.member_id;
+
+    var cards = [];
+    var services = [];
+    var deposits = [];
+    var coupons = [];
+
+    async.series([_queryCards, _queryServices, _queryDeposits, _queryCoupons], function(err){
+
+        if(err){
+            console.log(err);
+            next(err);
+            return;
+        }
+
+        var result = {
+            cards: cards,
+            services: services,
+            deposits: deposits,
+            coupons: coupons
+        }
+
+        doResponse(req, res, result);
+    });
+
+    function _queryCards(callback){
+
+        var sql = "select a.id as card_id, a.currentMoney, b.name, b.baseInfo_type from planx_graph.tb_membercard as a, planx_graph.tb_membercardcategory as b " +
+            "where a.memberCardCategoryId = b.id and a.memberId = :member_id and a.enterprise_id = :enterprise_id";
+
+        dbHelper.execSql(sql, {enterprise_id: enterprise_id, member_id: member_id}, function(err, result){
+
+            if(err){
+                callback(err);
+                return;
+            }
+
+            cards = result;
+
+            callback(null);
+        });
+    }
+
+    function _queryServices(callback){
+        callback(null);
+    }
+
+    function _queryDeposits(callback){
+        deposits.push("haha");
+        callback(null);
+    }
+
+    function _queryCoupons(callback){
+        coupons.push("xixi");
+        callback(null);
+    }
 }
