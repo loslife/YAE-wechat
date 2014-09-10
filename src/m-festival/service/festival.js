47129f96d429548d1a10f1551596055180ebbfea
kyfxbl <kyfxbl@gmail.com>
2014-09-10 15:53:16 +0800

    分享活动的流程框架代码

--- src/m-festival/service/festival.js
+++ src/m-festival/service/festival.js
@@ -1,5 +1,54 @@
+var dbHelper = require(FRAMEWORKPATH + "/utils/dbHelper");
+
 exports.list = list;
+exports.share = share;
 
 function list(req, res, next){
-    res.send("优惠活动列表");
+
+    var enterpriseId = req.params["enterpriseId"];
+
+    dbHelper.queryData("weixin_festivals", {enterprise_id: enterpriseId, state: 1}, function(err, result){
+
+        if(err){
+            console.log(err);
+            next(err);
+            return;
+        }
+
+        res.render("festivals", {enterprise_id: enterpriseId, menu: "festival", festivals: result});
+    });
+}
+
+function share(req, res, next){
+
+    var enterpriseId = req.params["enterpriseId"];
+    var festivalId = req.query["fid"];
+    var memberId = req.session.member_id;
+
+    pageCount();
+
+    if(memberId){
+        res.render("got", {enterprise_id: enterpriseId, menu: "festival"})
+    }else{
+
+        dbHelper.queryData("weixin_festivals", {enterprise_id: enterpriseId, id: festivalId}, function(err, result){
+
+            if(err){
+                console.log(err);
+                next(err);
+                return;
+            }
+
+            if(result.length === 0){
+                next({message: "活动不存在"});
+                return;
+            }
+
+            res.render("input", {enterprise_id: enterpriseId, menu: "festival", festival: result[0]});
+        });
+    }
+
+    function pageCount(){
+
+    }
 }
