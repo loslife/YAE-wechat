b8cb1e5f37818d1df0c99ce59c7a659c17b60781
kyfxbl <kyfxbl@gmail.com>
2014-09-11 17:01:19 +0800

    先隐藏了分享到朋友圈按钮，如果是在微信环境下打开，才展示此按钮

--- src/m-festival/static/gotAndShare.js
+++ src/m-festival/static/gotAndShare.js
@@ -1,29 +1,36 @@
 $(function(){
 
-    alert(YLS.openInWeixin());
+    $("#btn_share").hide();
 
     WeixinApi.ready(function(Api) {
 
-        var enterpriseId = $("#enterprise_id").text();
-        var festivalId = $("#festival_id").text();
+        $("#btn_share").show();
 
-        var shareURL = "http://121.40.75.73/svc/wsite/" + enterpriseId + "/getPresent?fid=" + festivalId;
+        attachShareCallback();
 
-        var wxData = {
-            "imgUrl" : "http://121.40.75.73/resource/share_thumb.jpg",
-            "link" : shareURL,
-            "desc" : "优惠活动描述",
-            "title" : "优惠活动标题"
-        };
+        function attachShareCallback(){
 
-        var wxCallbacks = {
+            var enterpriseId = $("#enterprise_id").text();
+            var festivalId = $("#festival_id").text();
 
-            confirm: function(resp) {
-                alert("分享成功");
-            }
-        };
+            var shareURL = "http://121.40.75.73/svc/wsite/" + enterpriseId + "/getPresent?fid=" + festivalId;
 
-        Api.shareToFriend(wxData, wxCallbacks);
-        Api.shareToTimeline(wxData, wxCallbacks);
+            var wxData = {
+                "imgUrl" : "http://121.40.75.73/resource/share_thumb.jpg",
+                "link" : shareURL,
+                "desc" : "优惠活动描述",
+                "title" : "优惠活动标题"
+            };
+
+            var wxCallbacks = {
+
+                confirm: function(resp) {
+                    alert("分享成功");
+                }
+            };
+
+            Api.shareToFriend(wxData, wxCallbacks);
+            Api.shareToTimeline(wxData, wxCallbacks);
+        }
     });
 });
