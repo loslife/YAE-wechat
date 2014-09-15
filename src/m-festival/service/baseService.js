9b026b4890a6569b9bfeea93ae67a768829410b2
kyfxbl <kyfxbl@gmail.com>
2014-09-15 16:50:21 +0800

    bug fixed

--- src/m-festival/service/baseService.js
+++ src/m-festival/service/baseService.js
@@ -95,12 +95,12 @@ function providePresent(enterpriseId, festivalId, memberId, phone, callback){
             if(memberId){
                 callback(null);
             }else{
-                _generateSecurityCode(festival, callback);
+                _generateSecurityCode(result, callback);
             }
         });
     });
 
-    function _generateSecurityCode(festival, callback){
+    function _generateSecurityCode(model, callback){
 
         var code = Math.round(900000 * Math.random() + 100000) + "";
 
@@ -108,9 +108,9 @@ function providePresent(enterpriseId, festivalId, memberId, phone, callback){
 
         function _saveCode(callback){
 
-            festival.security_code = code;
+            model.security_code = code;
 
-            dbHelper.updateByID("weixin_present_received", festival, function(err, result){
+            dbHelper.updateByID("weixin_present_received", model, function(err, result){
                 callback(err);
             });
         }
