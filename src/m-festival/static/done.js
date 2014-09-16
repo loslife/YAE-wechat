a4cf17a1a79f3469fd68c34bfcc20878ac3b33d5
kyfxbl <kyfxbl@gmail.com>
2014-09-16 21:59:16 +0800

    done页面的高度

--- src/m-festival/static/done.js
+++ src/m-festival/static/done.js
@@ -1,8 +1,38 @@
-$(function(){
+if(WeixinApi.openInWeixin()){
+
+    if (typeof WeixinJSBridge == "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener('WeixinJSBridgeReady', init, false);
+        } else if (document.attachEvent) {
+            document.attachEvent('WeixinJSBridgeReady', init);
+            document.attachEvent('onWeixinJSBridgeReady', init);
+        }
+    } else {
+        init();
+    }
+
+}else{
+
+    $(function(){
+        init();
+    });
+}
+
+function init(){
+
+    (function(){
+
+        var backH = $("#back").height();
+        var windowH = $(window).height();
+        var doneHeight = windowH - backH;
+
+        $("#m-festival-done").height(doneHeight);
+
+    })();
 
     $("#back").on("click", function ($even) {
         location.href = "festival";
         $even.stopPropagation();
     });
+}
 
-});
