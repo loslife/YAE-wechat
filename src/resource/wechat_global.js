491534416cdaca845d7bff12bdb838b9145c80f4
kyfxbl <kyfxbl@gmail.com>
2014-09-18 20:03:31 +0800

    菜单的位置，现在不用算高度了

--- src/resource/wechat_global.js
+++ src/resource/wechat_global.js
@@ -3,32 +3,3 @@ var g_env = {
     check_code_url: "http://192.168.1.106:3000/svc/checkCode/",
     binding_url: "http://192.168.1.106:5000/svc/wsite/"
 };
-
-function adjustMenuPosition(){
-
-    var menuH = $("#menu").height();
-    var windowH = $(window).height();
-    var contentH = windowH - menuH;
-
-    $("#content").height(contentH);
-}
-
-if(WeixinApi.openInWeixin()){
-
-    if (typeof WeixinJSBridge == "undefined") {
-        if (document.addEventListener) {
-            document.addEventListener('WeixinJSBridgeReady', adjustMenuPosition, false);
-        } else if (document.attachEvent) {
-            document.attachEvent('WeixinJSBridgeReady', adjustMenuPosition);
-            document.attachEvent('onWeixinJSBridgeReady', adjustMenuPosition);
-        }
-    } else {
-        adjustMenuPosition();
-    }
-
-}else{
-
-    $(function(){
-        adjustMenuPosition();
-    });
-}
