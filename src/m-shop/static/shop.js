211302b65659c1a8a650f28e190d0952b8f8b242
kyfxbl <kyfxbl@gmail.com>
2014-09-27 15:44:32 +0800

    env

---
+++ src/m-shop/static/shop.js
@@ -0,0 +1,37 @@
+if(WeixinApi.openInWeixin()){
+
+    if (typeof WeixinJSBridge == "undefined") {
+        if (document.addEventListener) {
+            document.addEventListener('WeixinJSBridgeReady', init, false);
+        } else if (document.attachEvent) {
+            document.attachEvent('WeixinJSBridgeReady', init);
+            document.attachEvent('onWeixinJSBridgeReady', init);
+        }
+    } else {
+        init();
+    }
+
+}else{
+
+    $(function(){
+        init();
+    });
+}
+
+function init(){
+
+    var storeAddr = $("#store_addr").text();
+
+    var map = new BMap.Map("enterpriseMap");
+    var myGeo = new BMap.Geocoder();
+
+    myGeo.getPoint(storeAddr, function (point) {
+
+        if (point) {
+            map.centerAndZoom(point, 14);
+            map.addOverlay(new BMap.Marker(point));
+        } else {
+            $("#mapDiv").addClass("none");
+        }
+    }, "");
+}
