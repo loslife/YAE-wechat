420a13c14d41ebf6677ee1317d084e29ef49ea68
h00063542 <h00063542@gmail.com>
2014-10-11 03:08:37 +0800

    修改使用全局的域名访问

--- src/resource/wechat_global.js
+++ src/resource/wechat_global.js
@@ -1,5 +1,5 @@
 var g_env = {
-    security_code_url: "/svc/getCode/",
-    check_code_url: "/svc/checkCode/",
-    binding_url: "/svc/wsite/"
+    security_code_url: "http://www.yilos.com/svc/getCode/",
+    check_code_url: "http://www.yilos.com/svc/checkCode/",
+    binding_url: "http://www.yilos.com/svc/wsite/"
 };
