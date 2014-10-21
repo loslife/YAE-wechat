79c965d136e9849231127bf130ee250e7f3ab0b4
kyfxbl <kyfxbl@gmail.com>
2014-10-21 13:05:08 +0800

    #2002

--- src/resource/wechat_global.js
+++ src/resource/wechat_global.js
@@ -1,5 +1,5 @@
 var g_env = {
     security_code_url: global["_g_server"].serviceurl +  "/getCode/",
     check_code_url: global["_g_server"].serviceurl + "/checkCode/",
-    binding_url: global["_g_server"].serviceurl + "/wsite/"
+    binding_url: global["_g_server"].wxserviceurl + "/wsite/"
 };
