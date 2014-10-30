0b089d0e1f87238494ab3a1420cc292d42ceb8db
kyfxbl <kyfxbl@gmail.com>
2014-10-30 16:33:56 +0800

    会员卡信息，查询店铺名称

--- src/interface/service/memberService.js
+++ src/interface/service/memberService.js
@@ -11,6 +11,8 @@ if(global["_g_topo"].env === "dev"){
     server_address = "http://wx.yilos.com/";
 }
 
+var baseurl = global["_g_clusterConfig"].baseurl;
+
 exports.hasMemberBinding = hasMemberBinding;
 exports.queryCardsByCondition = queryCardsByCondition;
 
@@ -55,12 +57,11 @@ function queryCardsByCondition(condition, callback){
             var enterprise_id = item.enterprise_id;
             var member_id = item.member_id;
 
-            var cardServiceUrl = server_address + "svc/wsite/" + enterprise_id + "/membercards";
+            var queryEnterpriseNameUrl = baseurl + "/enterprise/name/" + enterprise_id;
 
             var options = {
-                method: "POST",
-                uri: cardServiceUrl,
-                body: {member_id: member_id},
+                method: "GET",
+                uri: queryEnterpriseNameUrl,
                 json: true
             };
 
@@ -73,55 +74,83 @@ function queryCardsByCondition(condition, callback){
 
                 var code = body.code;
                 if(code !== 0){
-                    next({message: "查询会员信息失败"});
+                    next({errorCode: 500, errorMessage: "查询企业名称失败"});
                     return;
                 }
 
-                var messages = [];// 单个店铺的图文消息
-
-                var cards = body.result.cards;
-
-                var url = server_address + "svc/wsite/" + enterprise_id + "/member?m_id=" + member_id;
-
-                if(cards.length === 0){
-
-                    var no_cards = {
-                        title: "您在店内还没有会员卡",
-                        picUrl: server_address + "resource/news2.png",
-                        url: url
-                    };
-                    messages.push(no_cards);
-
-                }else{
-
-                    var header = {
-                        title: "我的余额，点击查看更多",
-                        picUrl: server_address + "resource/news2.png",
-                        url: url
-                    };
-                    messages.push(header);
-
-                    _.each(cards, function(item){
-
-                        var message = {
-                            picUrl: server_address + "resource/card_detail.png",
-                            url: url
-                        };
-
-                        if(item.type === "recharge"){
-                            message.title = item.name + "余额" + item.currentMoney.toFixed(1) + "元";
-                        }else if(item.type === "recordTimeCard"){
-                            message.title = item.name + "剩余" + item.remainingTimes + "次";
-                        }else{
-                            message.title = item.name + "截止到" + new Date(item.expired_time).Format("yy-MM-dd");
-                        }
-
-                        messages.push(message);
-                    });
+                var name = body.result;
+                if(_.isEmpty(name)){
+                    name = "未命名店铺";
                 }
 
-                records.push(messages);// 单店图文消息放入总图文消息集
-                next(null);
+                var cardServiceUrl = server_address + "svc/wsite/" + enterprise_id + "/membercards";
+
+                var options = {
+                    method: "POST",
+                    uri: cardServiceUrl,
+                    body: {member_id: member_id},
+                    json: true
+                };
+
+                request(options, function(err, response, body) {
+
+                    if(err){
+                        next(err);
+                        return;
+                    }
+
+                    var code = body.code;
+                    if(code !== 0){
+                        next({message: "查询会员信息失败"});
+                        return;
+                    }
+
+                    var messages = [];// 单个店铺的图文消息
+
+                    var cards = body.result.cards;
+
+                    var url = server_address + "svc/wsite/" + enterprise_id + "/member?m_id=" + member_id;
+
+                    if(cards.length === 0){
+
+                        var no_cards = {
+                            title: "您在" + name + "店内还没有会员卡",
+                            picUrl: server_address + "resource/news2.png",
+                            url: url
+                        };
+                        messages.push(no_cards);
+
+                    }else{
+
+                        var header = {
+                            title: "你在" + name + "店内的会员卡信息，点击查看更多",
+                            picUrl: server_address + "resource/news2.png",
+                            url: url
+                        };
+                        messages.push(header);
+
+                        _.each(cards, function(item){
+
+                            var message = {
+                                picUrl: server_address + "resource/card_detail.png",
+                                url: url
+                            };
+
+                            if(item.type === "recharge"){
+                                message.title = item.name + "余额" + item.currentMoney.toFixed(1) + "元";
+                            }else if(item.type === "recordTimeCard"){
+                                message.title = item.name + "剩余" + item.remainingTimes + "次";
+                            }else{
+                                message.title = item.name + "截止到" + new Date(item.expired_time).Format("yy-MM-dd");
+                            }
+
+                            messages.push(message);
+                        });
+                    }
+
+                    records.push(messages);// 单店图文消息放入总图文消息集
+                    next(null);
+                });
             });
 
         }, function(err){
